91bacb4f461a1598e99f6f0e5374d8d918a61e76
KienBui2k <haikien371316@gmail.com>
2023-10-16 22:36:17 +0700

    fix bug and create device socket

---
+++ src/modules/socket/socket.module.ts
@@ -0,0 +1,13 @@
+import { Module } from "@nestjs/common";
+import { DeviceSocketGateway } from "./devices/device.socket";
+import { JwtService } from "../jwt/jwt.service";
+
+@Module({
+    imports: [],
+    providers: [DeviceSocketGateway,JwtService, 
+     {provide: 'MCS_HOST', useValue: process.env.MCS_HOST}
+],
+    
+})
+
+export class socketModule {}
